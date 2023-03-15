98186221f5cd7b3ca2451d6bc18f0f89e15f8fa5
Eric Koncina <mail@koncina.eu>
2023-03-15 12:13:55 +0100

    add semi joining methods to filter out features or samples based on another tibble

--- R/features.R
+++ R/features.R
@@ -31,6 +31,42 @@ filter_features.SummarizedExperiment <- function(x, ...) {
     (\(f) x[f$.feature,])()
 }
 
+#' Filter out features of interest using semi_join()
+#'
+#' @importFrom dplyr semi_join
+#' @import tidyselect
+#'
+#' @param x A SummarizedExperiment or ExpressionSet object.
+#' @param y A tibble or data.frame
+#' @param by A character vector of variables to join by.
+#' @param ... Arguments passed to filter
+#'
+#' @export
+semi_join_features <- function (x, y, by = NULL, ...) {
+  UseMethod("semi_join_features", x)
+}
+
+#' @importFrom Biobase pData
+#'
+#' @export
+semi_join_features.ExpressionSet <- function(x, y, by = NULL, ...) {
+  Biobase::fData(x) |>
+    as_tibble(rownames = ".feature") |>
+    semi_join(y, by = {{by}}, ...) |>
+    (\(f) x[f$.feature,])()
+}
+
+#' @importFrom SummarizedExperiment colData
+#'
+#' @export
+semi_join_features.SummarizedExperiment <- function(x, y, by = NULL, ...) {
+  SummarizedExperiment::rowData(x) |>
+    as_tibble(rownames = ".feature") |>
+    semi_join(y, by = {{by}}, ...) |>
+    (\(f) x[f$.feature,])()
+}
+
+
 #' "Also known as" splits collapsed list of gene identifiers and tests if
 #' the expression is part of it (could be replaced by `str_detect()` or similar)
 #'
--- R/samples.R
+++ R/samples.R
@@ -32,6 +32,40 @@ filter_samples.SummarizedExperiment <- function(x, ...) {
     (\(i) x[, i$.sample])()
 }
 
+#' Filter out samples of interest using semi_join()
+#'
+#' @importFrom dplyr semi_join
+#' @import tidyselect
+#'
+#' @param x A SummarizedExperiment or ExpressionSet object.
+#' @param y A tibble or data.frame
+#' @param by A character vector of variables to join by.
+#' @param ... Arguments passed to filter
+#'
+#' @export
+semi_join_samples <- function (x, y, by = NULL, ...) {
+  UseMethod("semi_join_samples", x)
+}
+
+#' @importFrom Biobase pData
+#'
+#' @export
+semi_join_samples.ExpressionSet <- function(x, y, by = NULL, ...) {
+  Biobase::pData(x) |>
+    as_tibble(rownames = ".sample") |>
+    semi_join(y, by = {{by}}, ...) |>
+    (\(i) x[, i$.sample])()
+}
+
+#' @importFrom SummarizedExperiment colData
+#'
+#' @export
+semi_join_samples.SummarizedExperiment <- function(x, y, by = NULL, ...) {
+  SummarizedExperiment::colData(x) |>
+    as_tibble(rownames = ".sample") |>
+    semi_join(y, by = {{by}}, ...) |>
+    (\(i) x[, i$.sample])()
+}
 
 #' Extract available samples as a tibble
 #' A wrapper around colData() for SummarizedExperiment
